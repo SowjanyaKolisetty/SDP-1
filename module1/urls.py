5771d086a8e2405c06152821fdb41f892e656efe
SowjanyaKolisetty <2200030823@kluniversity.in>
2024-02-22 12:01:42 +0530

    SOWJANYA

---
+++ module1/urls.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path("hello1", hello1),
+    path("hello/",hello,name='hello'),
+    path("",Newhomepage,name='Newhomepage'),
+    path("card/",travelpackage,name='travelpackage'),
+    path("p/",print_to_console,name='print_to_console'),
+    path("print1/",print1,name="print1"),
+    path('ran1/',random123,name="random123"),
+    path("h1/",ran,name="ran"),
+    path("context/",randomotp,name='Randomotp'),
+    path("h2/",getdate1,name="getdate1"),
+    path("date/",get_date,name="get_date"),
+    path("py/",tzfunctioncall,name="tzfunctioncall"),
+    path("data/",data,name="data"),
+    path("register/",registerloginfunction,name="registerloginfunction"),
+    path("chart/",pie_chart,name="Pie_Chart"),
+    path("photo/",Photos,name="Photos"),
+    path("w/",weathercall,name="weathercall"),
+    path("wh/",weatherlogic,name="weatherlogic"),
+    path("login/",login,name="login"),
+    path("signup/",signup,name="signup"),
+    path("logout/",logout,name="logout"),
+    path("login1/",login1,name="login1"),
+    path("signup1/",signup1,name="signup1"),
+    path("contactmail/",contactmail,name="contactmail"),
+    path("contact/",contact,name="contact"),
+]
